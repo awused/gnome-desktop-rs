4118ca0f0294d4307a75658d319baba077752f38
Victor Fuentes <vmfuentes64@gmail.com>
2022-12-07 04:09:44 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk-lib = naersk.lib."${system}";
+      in
+      {
+        packages.snow = naersk-lib.buildPackage {
+          pname = "snow";
+          root = ./.;
+          nativeBuildInputs = with pkgs; [ makeWrapper ];
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+            sqlite
+          ];
+          postInstall = ''
+            wrapProgram $out/bin/snow --prefix PATH : '${pkgs.lib.makeBinPath [ pkgs.sqlite ]}'
+            mkdir -p $out/libexec
+            mv $out/bin/snow-helper $out/libexec/snow-helper
+          '';
+        };
+
+        defaultPackage = self.packages.${system}.snow;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            cargo-tarpaulin
+            clippy
+            openssl
+            pkg-config
+            rust-analyzer
+            rustc
+            rustfmt
+            sqlite
+            gnome-desktop
+            gtk4
+          ];
+        };
+      });
+}
